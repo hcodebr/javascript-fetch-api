1d2faa3a4a5aaa16e995c8c0fb975b1cfcd454c2
Glaucio Daniel <glauciodaniel@gmail.com>
2020-03-05 13:32:34 -0300

    Projeto Fetch API

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+const cep = document.querySelector("#cep")
+
+const showData = (result)=>{
+    for(const campo in result){
+        if(document.querySelector("#"+campo)){
+            document.querySelector("#"+campo).value = result[campo]
+        }
+    }
+}
+
+
+
+
+cep.addEventListener("blur",(e)=>{
+    let search = cep.value.replace("-","")
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+
+    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+    .then(response =>{ response.json()
+        .then( data => showData(data))
+    })
+    .catch(e => console.log('Deu Erro: '+ e,message))
+})
